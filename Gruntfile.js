9d3d52add88ea017930ed0e0bbe15b19ee695ade
Ricky Walker <ricky.walker@jediq.com>
2015-03-11 18:24:15 +0000

    changed solr to lunr

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*
+ * grunt-lunr-index-generator
+ * https://github.com/jediq/documento/grunt-lunr-index-generator
+ *
+ * Copyright (c) 2015 Ricky Walker
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    lunr_index_generator: {
+        options: {
+          src: ['test/fixtures/docs'],
+          dest: 'test/fixtures/docs'
+        }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'lunr_index_generator', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ test/fixtures/docs/document2.md
@@ -0,0 +1,3 @@
+# My second document title
+
+My second document text is also brilliant.   Second is better than first.
