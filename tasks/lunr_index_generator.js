f62a074b101a0dc0b9a18f9964db205f74b79dda
Ricky Walker <ricky.walker@jediq.com>
2015-03-11 18:41:39 +0000

    should really add the task :)

---
+++ tasks/lunr_index_generator.js
@@ -0,0 +1,21 @@
+/*
+ * grunt-lunr-index-generator
+ * https://github.com/jediq/documento/grunt-lunr-index-generator
+ *
+ * Copyright (c) 2015 Ricky Walker
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.log.ok('Processing plugin.');
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('lunr_index_generator', 'The best Grunt plugin ever.', function() {
+    grunt.log.ok('go go go.');
+  });
+
+};
